814dd86eca20f57ab2afee11790b89ddd0f0018d
Frannovara <you@example.com>
2021-04-24 17:42:03 -0300

    router

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-
+const taskRouter = require("./routes/task");
 const app = express();
 
 // view engine setup
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-
+app.use("/tasks", taskRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));

c37dad2f5eab683e5bca9d32541930bc8640eec6
Frannovara <you@example.com>
2021-04-26 16:06:54 -0300

    override

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const methodOverride = require('method-override');
 const indexRouter = require("./routes/index");
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(methodOverride('_method'));
 app.use("/", indexRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

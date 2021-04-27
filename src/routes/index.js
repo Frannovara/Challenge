6d87a606affeef39cf38025f3e869dcc4c997080
Frannovara <you@example.com>
2021-04-27 16:59:38 -0300

    router correccion

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 const mainController = require ('../controllers/mainController');
 /* GET users listing. */
-router.get("/", mainController.index)
-router.post("/tasks", mainController.task)
-router.get("/tasks", mainController.create)
-router.post("/", mainController.delete)
-router.get("/", mainController.read)
-router.get("/:id", mainController.detail)
+router.get("/tasks", mainController.task)
+router.post("/tasks", mainController.create)
+router.delete("/tasks/:id", mainController.delete)
+router.get("/tasks/:id", mainController.detail)
 module.exports = router;

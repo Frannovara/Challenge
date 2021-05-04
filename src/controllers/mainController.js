25455554c64d01f365403fba3912292b88372911
Frannovara <you@example.com>
2021-05-04 14:48:00 -0300

    validacion

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('../database/models');
 const db = require('../database/models');
 const Op = Sequelize.Op
+const { check, validationResult, body } = require('express-validator');
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -10,9 +11,20 @@ require('dotenv').config()
 const controller = {
 
 create: function(req, res) {
+    let errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+
     db.Tasks.create({
         task: req.body.title
     }).then(respuesta => res.redirect("/tasks"))
+} else {
+    db.Tasks.findAll()
+    .then(function(tasks){
+    res.render("tasks.ejs", {tasks: tasks, errors: errors.errors})
+    })
+}
+
 },
 
 delete: function(req, res) {
@@ -29,6 +41,7 @@ task: function(req, res){
     .then(function(tasks){
         res.render("tasks", {tasks:tasks})
     })
+
 },
 
 detail: function(req, res){
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const mainController = require ('../controllers/mainController');
+const { check, validationResult, body } = require('express-validator');
 /* GET users listing. */
 router.get("/tasks", mainController.task)
-router.post("/tasks", mainController.create)
+router.post("/tasks", [check('title').isLength({min: 3}).withMessage('La tarea debe contener mínimo 3 caracteres')], mainController.create)
 router.delete("/tasks/:id", mainController.delete)
 router.get("/tasks/:id", mainController.detail)
 module.exports = router;

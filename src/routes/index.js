b06f1b918ffc6c2f9c07327e0c939be3f8250a5f
Frannovara <you@example.com>
2021-05-04 15:06:24 -0300

    correccion ruta

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const mainController = require ('../controllers/mainController');
 const { check, validationResult, body } = require('express-validator');
 /* GET users listing. */
 router.get("/tasks", mainController.task)
-router.post("/tasks", [check('title').isLength({min: 3}).withMessage('La tarea debe contener mínimo 3 caracteres')], mainController.create)
+router.post("/tasks", [check('title').isLength({min: 3})], mainController.create)
 router.delete("/tasks/:id", mainController.delete)
 router.get("/tasks/:id", mainController.detail)
 module.exports = router;
